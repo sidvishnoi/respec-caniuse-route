f53eb8c1c63b07b3fcc8dfccc5f0b3b0f13b2416
Marcos Cáceres <marcos@marcosc.com>
2019-03-27 17:05:01 +1100

    fix(scraper): limit clone and pull depth to 1

--- scraper/get-data.sh
+++ scraper/get-data.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ -d "caniuse-data" ]; then
   cd caniuse-data
-  git pull origin master
+  git pull origin master --depth 1
   cd ..
 else
-  git clone https://github.com/Fyrd/caniuse.git caniuse-data
+  git clone --depth 1 https://github.com/Fyrd/caniuse.git caniuse-data
 fi
